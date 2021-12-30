f6279859560ee05fd31dfa75870d68de70af706d
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2021-12-30 20:31:16 +0200

    Add exeptions to log page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +36,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
-        });
+            Log::error('Allert message: ' . $e->getMessage() . ' In: ' . $e->getFile() . ':' . $e->getLine());
+        })->stop();
     }
 }
