810038039fc67d9a3b471b504532aab1a3416b63
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-11 00:29:23 +0200

    Add SettingFactory

---
+++ database/factories/Shop/Admin/Settings/SettingFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Shop\Admin\Settings;
+
+use App\Shop\Admin\Settings\Setting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class SettingFactory extends Factory
+{
+    protected $model = Setting::class;
+
+    public function definition()
+    {
+        return [
+            'setting' => Str::random(5), 
+            'value' => Str::random(5), 
+        ]; 
+    }
+}
