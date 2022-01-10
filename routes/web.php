c3c31cab760eb6ec7950a3380843966e47d11d3f
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-10 22:22:07 +0200

    Add routes for suppliers and settings

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,19 @@ Route::group(['prefix' => 'admin', 'middleware' => 'notAutorized', 'as' => 'admi
             Route::get('/logs', 'LogController@index')->name('index');
             Route::get('/clear-logs/{param}', 'LogController@clearLogFile')->name('clear');
         });
+        Route::group(['middleware' => 'permission:general-settings'], function(){
+            Route::get('/admin-settings', 'SettingsController@index')->name('generalSettings.index');
+            Route::post('/admin-settings/store', 'SettingsController@store')->name('generalSettings.store');
+        });
+    });
+    Route::group(['prefix' => 'catalog', 'as' => 'catalog.'], function () {
+        Route::group(['middleware' => 'permission:suppliers'], function(){
+            Route::get('/suppliers/restore/{id}', 'SupplierController@restore')->name('suppliers.restore');
+            Route::get('/suppliers/trash', 'SupplierController@trash')->name('suppliers.trash');
+            Route::delete('/suppliers/soft-delete/{id}', 'SupplierController@softDelete')->name('suppliers.delete');
+            Route::post('/suppliers/restore-all', 'SupplierController@restoreAll')->name('suppliers.massRestore');
+            Route::post('/suppliers/delete-all', 'SupplierController@forceDeleteAll')->name('suppliers.massDelete');
+            Route::resource('/suppliers', 'SupplierController'); 
+        });
     });
 });
