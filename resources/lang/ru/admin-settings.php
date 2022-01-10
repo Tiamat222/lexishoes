1b2a82776d1ff38b4587c5c44f5decd461336c02
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-10 23:53:29 +0200

    Add lang file for settings page

---
+++ resources/lang/ru/admin-settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'settings-success' => 'Общие настройки были успешно обновлены',
+    'settings-adminPage-required' => 'Поле "Alias админ-панели" обязательно к заполнению',
+    'settings-adminPage-regex' => 'Alias админ-панели может состоять только из целых чисел, знака "-" и букв латинского алфавита в нижнем регистре',
+    'settings-adminPage-min' => 'Минимальная длина alias админ-панели - 3 символа',
+    'settings-adminEmail-required' => 'Поле "Email администратора" обязательно к заполнению',
+    'settings-adminEmail-email' => 'Email администратора должен быть корректным',
+    'settings-storeLogo-image' => 'Логотип магазина должен быть изображением',
+    'settings-storeLogo-mimes' => 'Поддерживаемые расширения логотипа магазина - jpeg, png, jpg, gif',
+];
