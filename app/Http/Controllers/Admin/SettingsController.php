577f8b7de8c551a31f58696cadb75f63ed6b8973
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-10 22:25:24 +0200

    Add SettingsController

---
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Shop\Admin\Settings\Services\SettingsService;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+    /**
+     * SettingsService instance
+     *
+     * @var SettingsService
+     */
+    private $settingsService;
+ 
+    /**
+     * SettingsController constructor
+     *
+     * @var SettingsService
+     */
+    public function __construct(SettingsService $settingsService)
+    {
+        $this->settingsService = $settingsService;
+    }
+
+    /**
+     * Display a listing of the settings
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('admin-templates.settings.general-settings', [
+            'settingsList' => $this->settingsService->getAllSettingsToArray(),
+        ]);
+    }
+
+    /**
+     * Store settings
+     *
+     * @param Request $request
+     * 
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $this->settingsService->storeSettings($request->except(['_token', '_url']));
+        return redirect()->route('admin.settings.generalSettings.index');
+    }
+}
+
