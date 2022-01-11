57f215fae108f8d83ce4e4e3b2f307848438427d
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-11 13:40:25 +0200

    Changed styles for information blocks

--- resources/views/admin-templates/info-messages.blade.php
+++ resources/views/admin-templates/info-messages.blade.php
@@ -1,30 +1,36 @@
 @if($message = Session::get('warning_message'))
 <div class="alert alert-warning alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h5><i class="icon fas fa-exclamation-triangle"></i> Внимание!</h5>
-    {{ $message }}
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h5><i class="icon fas fa-exclamation-triangle"></i> Внимание!</h5>
+  {{ $message }}
 </div>
 @endif
 @if($message = Session::get('error_message'))
-<div class="alert alert-danger alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h5><i class="icon fas fa-ban"></i> Внимание!</h5>
-    {{ $message }}
+<div class="alert callout callout-danger" style="background:#FF8080;">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h5><i class="icon fas fa-ban"></i>Ошибка!</h5>
+    @if(count($message) >= 1)
+      @foreach($message as $errorMessage)
+        - {{ $errorMessage }}<br>
+      @endforeach
+    @else 
+      {{ $message }}
+    @endif
 </div>
 @endif
 @if($message = Session::get('success_message'))
-<div class="alert alert-success alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h5><i class="fas fa-fw fa-check-circle"></i> Успех!</h5>
-    {{ $message }}
+<div class="alert callout callout-success" style="background:#85BF73;">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h5><i class="icon fas fa-check"></i> Успех!</h5>
+  {{ $message }}         
 </div>
 @endif
 @if($errors->any())
-<div class="alert alert-danger alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h5><i class="icon fas fa-ban"></i> Внимание!</h5>
+<div class="alert callout callout-danger" style="background:#FF8080;">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+  <h5><i class="icon fas fa-ban"></i>Ошибка!</h5>
     @foreach($errors->all() as $error)
-        {{ $error }}</br>
+        - {{ $error }}</br>
     @endforeach
 </div>
 @endif
