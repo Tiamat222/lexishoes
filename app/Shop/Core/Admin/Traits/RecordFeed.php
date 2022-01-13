4a66444103d61a145a58b1c0d84c9a4d5152cdb8
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-13 15:51:47 +0200

    Added RecordFeed trait to record adminis actions. Added support for created, updated and deleted events

---
+++ app/Shop/Core/Admin/Traits/RecordFeed.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Shop\Core\Admin\Traits;
+
+use App\Shop\Admin\Feeds\Feed;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+trait RecordFeed
+{    
+    /**
+     * Boot records activity
+     *
+     * @return void
+     */
+    public static function booted(): void
+    {
+        foreach (static::getModelEvents() as $event) {
+            static::$event(function ($model) use ($event) {
+                $model->recordFeed($event);
+            });
+        }
+
+        static::deleted(function ($model) {
+            $model->feeds()->delete();
+            $model->recordFeed('force_deleted');
+        });
+    }
+    
+    /**
+     * Get model events
+     *
+     * @return array
+     */
+    protected static function getModelEvents(): array
+    {
+        return [
+            'created',
+            'updated',
+        ];
+    }
+    
+    /**
+     * Record feed in db
+     *
+     * @param  string $event
+     * @return void
+     */
+    private function recordFeed(string $event): void
+    {
+        $this->feeds()->create([
+            'admin_id' => auth()->guard('admins')->id(),
+            'type' => $event . '_' . strtolower(class_basename($this))
+        ]);
+    }
+    
+    /**
+     * Feeds 
+     *
+     * @return MorphMany
+     */
+    private function feeds(): MorphMany
+    {
+        return $this->morphMany(Feed::class, 'feedable');
+    }
+}
