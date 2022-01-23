db82a7841cb2d251794bbbc4ea371b7f92bfb081
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-23 14:24:33 +0200

    Add form request for category creating

---
+++ app/Shop/Admin/Categories/Requests/CreateCategoryRequest.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Shop\Admin\Categories\Requests;
+
+use App\Shop\Admin\Categories\Services\DTO\CategoryCreateDto;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'unique:categories',
+            ],
+            'slug' => [
+                'required', 
+                'regex:/^[a-z0-9-]*$/', 
+            ],
+            'category_image' => [
+                'image',
+                'mimes:jpeg,png,jpg,gif',
+            ]
+        ];
+    }
+
+    /**
+     * Validation messages.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => __('admincategories.category-name-required'),
+            'name.min' => __('admincategories.category-name-min'),
+            'name.unique' => __('admincategories.category-name-unique'),
+            'slug.required' => __('admincategories.category-slug-required'),
+            'slug.regex' => __('admincategories.category-slug-regex'),
+            'category_image.image' => __('admincategories.category-image-file'),
+            'category_image.mimes' => __('admincategories.category-image-mimes')
+        ]; 
+    }
+    
+    /**
+     * Category create DTO
+     *
+     * @return CategoryCreateDto
+     */
+    public function data(): CategoryCreateDto
+    {
+        return new CategoryCreateDto([
+            'is_active' => $this->input('is_active'), 
+            'name' => $this->input('name'), 
+            'slug' => $this->input('slug'), 
+            'description' => $this->input('description'), 
+            'category_image' => $this->file('category_image'), 
+            'meta_title' => $this->input('meta_title'), 
+            'meta_description' => $this->input('meta_description'), 
+            'meta_keywords' => $this->input('meta_keywords'),
+            'category_id' => $this->input('category_id')
+        ]);
+    }
+}
