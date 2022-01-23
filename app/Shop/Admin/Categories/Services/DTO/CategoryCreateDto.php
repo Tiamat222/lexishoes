26e88311a5bd0a0e075772beedbe3846da509752
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-23 14:22:18 +0200

    Add DTO for category creating

---
+++ app/Shop/Admin/Categories/Services/DTO/CategoryCreateDto.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Shop\Admin\Categories\Services\DTO;
+
+use Spatie\DataTransferObject\DataTransferObject;
+use Illuminate\Http\UploadedFile;
+
+final class CategoryCreateDto extends DataTransferObject
+{
+    /**
+     * Category status
+     *
+     * @var string
+     */
+    public string $is_active;
+
+    /**
+     * Category name
+     *
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * Category slug
+     *
+     * @var string
+     */
+    public string $slug;
+
+    /**
+     * Category description
+     *
+     * @var string|null
+     */
+    public string|null $description;
+
+    /**
+     * Category cover image
+     *
+     * @var
+     */
+    public UploadedFile|string|null $category_image;
+
+    /**
+     * Category meta-title
+     *
+     * @var string|null
+     */
+    public string|null $meta_title;
+
+    /**
+     * Category meta-description
+     *
+     * @var string|null
+     */
+    public string|null $meta_description;
+
+    /**
+     * Category meta-keywords
+     *
+     * @var string|null
+     */
+    public string|null $meta_keywords;
+
+    /**
+     * Parent category
+     *
+     * @var int|null
+     */
+    public int|null $category_id;
+}
