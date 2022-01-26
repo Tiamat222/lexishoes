9db14943303cc28c6ee11ac0a128e2a0f9965c27
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-01-26 14:29:28 +0200

    Add lang file for suppliers module

---
+++ resources/lang/ru/admin-suppliers.php
@@ -0,0 +1,26 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Supplier Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on supplier pages for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines.
+    |
+    */
+
+return [
+    'supplier-name-required' => 'Поле "Поставщик" обязательно к заполнению',
+    'supplier-name-unique' => 'Название поставщика должно быть уникальным',
+    'supplier-name-min' => 'Минимальная длина названия поставщика - 2 символа',
+    'supplier-description-max' => 'Максимальная длина комментария - 300 символов',
+    'supplier-store-success' => 'Новый поставщик был успешно добавлен',
+    'supplier-update-success' => 'Поставщие был успешно обновлен',
+    'supplier-trash-move' => 'Поставщик был перемещен в корзину',
+    'supplier-trash-restore' => 'Поставщик был успешно восстановлен',
+    'supplier-force-delete' => 'Поставщик был успешно удален',
+    'suppliers-was-restored' => 'Выбранные поставщики были успешно восстановленны',
+    'suppliers-was-deleted' => 'Выбранные поставщики были удалены безвозвратно',
+]; 
