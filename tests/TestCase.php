cce5e0162cd909f0dce9338deb2633dd007271be
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-02-20 12:05:10 +0200

    validation logic for unit tests migrated to the TestCase.php + add permissions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,7 @@ use App\Shop\Admin\Admins\Admin;
 use App\Shop\Admin\Permissions\Permission;
 use App\Shop\Admin\Permissions\Services\PermissionService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Validation\Validator;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -25,6 +26,20 @@ abstract class TestCase extends BaseTestCase
      */
     protected $permissionService;
 
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    protected $rules;
+    
+    /**
+     * Validator
+     *
+     * @var \Illuminate\Validation\Factory
+     */
+    protected $validator;
+
     public function setUp(): void
     {
         parent::setUp();
@@ -39,7 +54,9 @@ abstract class TestCase extends BaseTestCase
             2 => 'log',
             3 => 'suppliers',
             4 => 'settings',
-            5 => 'categories'
+            5 => 'categories',
+            6 => 'export',
+            7 => 'attributes'
         ];
 
         foreach($permissions as $key => $value) {
@@ -47,5 +64,34 @@ abstract class TestCase extends BaseTestCase
             $selectedPermission = $this->permissionService->getPermissionById($key);
             $this->admin->permissions()->attach($selectedPermission);
         }
+    }
+
+    /**
+     * Fields validation
+     *
+     * @param  string $field
+     * @param  string $value
+     * 
+     * @return Validator
+     */
+    protected function getFieldValidator(string $field, string $value): Validator
+    {
+        return $this->validator->make(
+            [$field => $value],
+            [$field => $this->rules[$field]]
+        );
+    }
+    
+    /**
+     * Get fields for validation
+     *
+     * @param  string $field
+     * @param  string $value
+     * 
+     * @return bool
+     */
+    protected function validateField(string $field, string $value): bool
+    {
+        return $this->getFieldValidator($field, $value)->passes();
     }
 }
