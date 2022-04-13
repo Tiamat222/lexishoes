419a17fbfc59ef525d015e11c4d7b4b3eb42483d
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-13 09:25:48 +0300

    Added functionality for outputting admins feeds

--- app/Shop/Admin/Admins/Admin.php
+++ app/Shop/Admin/Admins/Admin.php
@@ -2,11 +2,13 @@
 
 namespace App\Shop\Admin\Admins;
 
+use App\Shop\Admin\Feeds\Feed;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Shop\Admin\Permissions\Permission;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Admin extends Authenticatable
 {
@@ -48,10 +50,31 @@ class Admin extends Authenticatable
      * Admin has permission
      *
      * @param  string $permission
+     * 
      * @return bool
      */
     public function hasPermission(string $permission): bool
     {
         return (bool) $this->permissions->where('slug', $permission)->count();
+    }
+
+    /**
+     * Admin has feeds
+     *
+     * @return HasMany
+     */
+    public function feeds(): HasMany
+    {
+        return $this->hasMany(Feed::class)->where('created_at','>', \Carbon\Carbon::now()->addDays(-100));
+    }
+    
+    /**
+     * Latest feeds
+     *
+     * @return HasMany
+     */
+    public function latestFeeds(): HasMany
+    {
+        return $this->hasMany(Feed::class)->where('created_at','>', \Carbon\Carbon::now()->addDays(-2));
     }
 }
