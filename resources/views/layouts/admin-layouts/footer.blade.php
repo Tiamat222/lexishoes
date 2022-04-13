bc71ed0642994bd3fe37b531a5e9573f46fb1129
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-13 09:02:52 +0300

    Redundant code was removed

--- resources/views/layouts/admin-layouts/footer.blade.php
+++ resources/views/layouts/admin-layouts/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.4
-    </div>
+    <strong>2021-2022 <a href="{{ env('APP_URL_STORE') }}">lexishoes.com.ua</a></strong>
 </footer>
