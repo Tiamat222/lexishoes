f7c98365f765fc4cf66d14899f79bdfd95ac04e2
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-13 09:34:01 +0300

    Change html template for admin login page

--- resources/views/admin-templates/login.blade.php
+++ resources/views/admin-templates/login.blade.php
@@ -1,42 +1,52 @@
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 3 | Log in</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ url('admin-template/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ url('admin-template/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('admin-template/dist/css/adminlte.min.css') }}">
-    <link rel="stylesheet" href="{{ url('admin-template/dist/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ url('admin-template/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('admin-template/dist/css/style.css') }}">
-    <title>Вход в административную панель</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   </head>
-  <body>
-  <div class="d-md-flex half">
-      <div class="contents">
-        <div class="container">
-          <div class="row align-items-center justify-content-center">
-            <div class="col-md-12">
-              <div class="form-block mx-auto">
-                <form action="{{ route('admin.check') }}" method="post">
-                  {{ csrf_field() }}
-                  <div class="form-group first">
-                    <label for="username">Логин</label>
-                    <input type="text" class="form-control" placeholder="Ваш логин" id="login">
-                  </div>
-                  <div class="form-group last mb-3">
-                    <label for="password">Пароль</label>
-                    <input type="password" class="form-control" placeholder="Ваш пароль" id="password">
-                  </div>
-                  <div class="d-sm-flex mb-5 align-items-center">
-                    <span class="ml-auto"><a href="#" class="forgot-pass">Забыл пароль</a></span> 
-                  </div>
-                  <input type="submit" value="Войти" class="btn btn-block btn-secondary">
-                </form>
-              </div>
+<body class="login-page" style="min-height: 512.781px;">
+<div class="login-box">
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Вход в админ панель</p>
+      @include('admin-templates.info-messages')
+      <form action="{{ route('admin.check') }}" method="post">
+        {{ csrf_field() }}
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Ваш логин" name="login">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
             </div>
           </div>
         </div>
-      </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Ваш пароль" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <a href="{{ route('admin.forget.get') }}">Забыл(а) пароль</a>
+            </div>
+          </div>
+          <div class="col-4">
+            <button type="submit" class="btn btn-block btn-secondary">Войти</button>
+          </div>
+        </div>
+      </form>
     </div>
-  </body>
+  </div>
+</div>
+</body>
 </html>
