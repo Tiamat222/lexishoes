90434ef947151c2a15fe6038aae987cda895aebf
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-13 09:17:30 +0300

    Added functionality for outputting the name and logo of the store

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Shop\Admin\Settings\Services\SettingsService;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -22,8 +23,27 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(SettingsService $settingsService)
     {
         Schema::defaultStringLength(191);
+
+        if(Schema::hasTable('general_settings')){
+            $storeLogo = $settingsService->getAllSettingsToArray()['store_logo'];
+            if(isset($storeLogo) && $storeLogo === '') {
+                $storeLogo = 'storage/images/default-images/default-store.jpg';
+            }
+            
+            $storeTitle = $settingsService->getAllSettingsToArray()['store_title'];
+            if(isset($storeTitle) && $storeTitle === '') {
+                $storeTitle = 'Нет названия';
+            }
+
+            view()->composer('layouts.admin-layouts.sidebar', function($view) use ($storeLogo, $storeTitle) {
+                $view->with([
+                    'storeLogo' => $storeLogo,
+                    'storeTitle' => $storeTitle,
+                ]);
+            });
+        }
     }
 }
