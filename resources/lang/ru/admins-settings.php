d9e4715c19038df64dec64fdb9b3d3cd1cfdd4d4
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-23 16:32:03 +0300

    Add lang file for admins settings module

---
+++ resources/lang/ru/admins-settings.php
@@ -0,0 +1,37 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admins Settings Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on admins settings pages for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines.
+    |
+    */
+
+return [
+    'admins-soft-delete-success' => 'Администратор был успешно перемещен в корзину',
+    'admins-login-required' => 'Поле "Логин" обязательно к заполнению.',
+    'admins-login-min' => 'Длина логина должна быть в диапазоне от 3-х до 20-ти символов.',
+    'admins-login-max' => 'Длина логина должна быть в диапазоне от 3-х до 20-ти символов.',
+    'admins-login-unique' => 'Администратор с таким логином уже зарегистрирован. Выберите другой.',
+    'admins-email-required' => 'Поле "Email" обязательно к заполнению.',
+    'admins-email-email' => 'Вы ввели некорректный адрес электронной почты.',
+    'admins-email-unique' => 'Администратор с таким email уже зарегистрирован. Укажите другой email.',
+    'admins-telephone-required' => 'Поле "Телефон" обязательно к заполнению.',
+    'admins-telephone-regex' => 'Номер телефона должен соответствовать формату +38(999) 999-99-99.',
+    'admins-newPwd-required' => 'Поле "Пароль" обязательно к заполнению.',
+    'admins-newPwd-min' => 'Минимальная длина пароля - 6 символов.',
+    'admins-newPwd-same' => 'Пароли должны совпадать.',
+    'admins-confirmPwd-required' => 'Поле "Подтвердите пароль" обязательно к заполнению.',
+    'admins-image-image' => 'Разрешено загружать только изображения.',
+    'admins-image-mimes' => 'Поддерживаются следующие расширения изображений - jpeg, png, jpg, gif.',
+    'admins-permissions-required' => 'Выдайте администратору права доступа.',
+    'admins-permissions-regex' => 'Неправильный формат прав доступа.',
+    'admins-destroy-success' => 'Администратор был успешно удален (безвозвратно).',
+    'admins-restore-success' => 'Администратор был успешно восстановлен',
+    'admins-create-new-admin' => 'Новый администратор был успешно создан.',
+    'admins-update-success' => 'Данные администратора были успешно обновлены.'
+];
