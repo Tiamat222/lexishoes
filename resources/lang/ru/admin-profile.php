a8af11e13d7416e2088508c4afcdff9cf6bc43c0
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-23 23:59:03 +0300

    Create lang file for admin profile pages

---
+++ resources/lang/ru/admin-profile.php
@@ -0,0 +1,34 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin profile Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used on admin profile page for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines.
+    |
+    */
+
+return [
+    'profile-login-required'      => 'Поле "Логин" обязательно к заполнению.',
+    'profile-login-unique'        => 'Администратор с таким логином уже зарегистрирован. Выберите другой.',
+    'profile-login-min'           => 'Минимальная длина логина - 3 символа.',
+    'profile-login-max'           => 'Максимальная длина логина - 12 символов.',
+    'profile-email-required'      => 'Поле "Email" обязательно к заполнению.',
+    'profile-email-email'         => 'Вы ввели некорректный адрес электронной почты.',
+    'profile-email-unique'        => 'Такой email уже существует.',
+    'profile-telephone-required'  => 'Поле "Телефон" обязательно к заполнению.',
+    'profile-telephone-unique'    => 'Администратор с таким номером телефона уже зарегистрирован. Укажите другой номер телефона.',
+    'profile-telephone-regex'     => 'Номер телефона должен соответствовать формату +38(111) 111-11-11.',
+    'profile-avatar-image'        => 'Загружать можно только изображения.',
+    'profile-avatar-mimes'        => 'Допустимые расширения изображений: jpeg, png, jpg, gif.',
+    'profile-newPwd-required'     => 'Поле "Новый пароль" обязательно к заполнению.',
+    'profile-newPwd-min'          => 'Минимальная длина нового пароля - 6 символов.',
+    'profile-newPwd-same'         => 'Пароли должны совпадать.',
+    'profile-confirmPwd-required' => 'Поле "Подтвердите пароль" обязательно к заполнению.',
+    'profile-data-success'        => 'Данные были успешно обновлены.',
+    'profile-pwd-success'         => 'Пароль был успешно изменен.',
+    'profile-pwd-compare'         => 'Новый пароль должен отличаться от старого.',
+];
