b670b75b19e4474b4db5beb4c36a3b603d27c4db
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-26 00:57:18 +0300

    Add new asserts. Delete settings arrays from file

--- tests/Feature/Admin/Settings/SettingsFeatureTest.php
+++ tests/Feature/Admin/Settings/SettingsFeatureTest.php
@@ -2,18 +2,19 @@
 
 namespace Tests\Feature\Admin\Suppliers;
 
-use App\Shop\Admin\Admins\Admin;
-use App\Shop\Admin\Settings\Services\SettingsService;
 use App\Shop\Admin\Settings\Setting;
-use App\Shop\Admin\Permissions\Permission;
-use App\Shop\Admin\Permissions\Services\PermissionService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class GeneralSettingsFeatureTest extends TestCase
 {
     use RefreshDatabase;
-
+    
+    /**
+     * setUp
+     *
+     * @return void
+     */
     public function setUp(): void
     {
         parent::setUp();
@@ -22,74 +23,59 @@ class GeneralSettingsFeatureTest extends TestCase
     /** @test **/
     public function settings_page_can_be_shown()
     {
-        $setting = [
-            'store_title',
-            'store_logo',
-            'admin_page',
-            'admin_email',
-            'site_language'
-        ];
-
-        $values = [
-            'Store title',
-            '',
-            'admin-alias',
-            'test@gmail.com',
-            'rus'
-        ];
-
-        for($i=0; $i <= count($setting)-1; $i++){
-            Setting::create([
-                'setting' => $setting[$i],
-                'value' => $values[$i],
-            ]);
-        }
-
         $this
-            ->actingAs($this->admin, 'admins')
             ->get(route('admin.settings.index'))
             ->assertStatus(200)
-            ->assertSee($values[0])
-            ->assertSee($values[2])
-            ->assertSee($values[3])
-            ->assertSee($values[4]);
+            ->assertSessionHasNoErrors()
+            ->assertSee(get_setting('admin_email'))
+            ->assertSee(get_setting('site_language'))
+            ->assertSee(get_setting('items_per_page'))
+            ->assertSee(get_setting('store_logo'))
+            ->assertSee(get_setting('store_title'))
+            ->assertSee(get_setting('pwd_length'))
+            ->assertViewHas('settingsList');
     }
 
     /** @test **/
     public function general_settings_can_be_updated()
     {
-        $setting = [
-            'store_title',
-            'store_logo',
-            'admin_page',
-            'admin_email',
-            'site_language'
-        ];
-
-        $values = [
-            'Store title',
-            '',
-            'admin-alias',
-            'test@gmail.com',
-            'rus'
-        ];
-
-        for($i=0; $i <= count($setting)-1; $i++){
-            Setting::create([
-                'setting' => $setting[$i],
-                'value' => $values[$i],
-            ]);
-        }
-
         $this
-            ->actingAs($this->admin, 'admins')
             ->post(route('admin.settings.store'), [
                 'target' => 'general_settings',
                 'store_title' => 'New title',
-                'admin_page' => 'newalias',
                 'admin_email' => 'new@mail.com',
-                'site_language' => 'ua'])
+                'site_language' => 'ua'
+            ])
             ->assertStatus(302)
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('admin.settings.index'))
+            ->assertSessionHas(['success_message' => __('admin-settings.settings-success')]);
+    }
+
+    /** @test **/
+    public function admin_settings_can_be_updated()
+    {
+        $this
+            ->post(route('admin.settings.store'), [
+                'target' => 'admin_settings',
+                'items_per_page' => 5
+            ])
+            ->assertStatus(302)
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('admin.settings.index'))
+            ->assertSessionHas(['success_message' => __('admin-settings.settings-success')]);
+    }
+
+    /** @test **/
+    public function user_settings_can_be_updated()
+    {
+        $this
+            ->post(route('admin.settings.store'), [
+                'target' => 'user_settings',
+                'pwd_length' => 6
+            ])
+            ->assertStatus(302)
+            ->assertSessionHasNoErrors()
             ->assertRedirect(route('admin.settings.index'))
             ->assertSessionHas(['success_message' => __('admin-settings.settings-success')]);            
     }
