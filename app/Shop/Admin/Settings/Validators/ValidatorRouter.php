fa8eeaebe3cd0764b6a8942185217c44a89ba9a5
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-26 01:06:37 +0300

    Add new cases. Add validation method

--- app/Shop/Admin/Settings/Validators/ValidatorRouter.php
+++ app/Shop/Admin/Settings/Validators/ValidatorRouter.php
@@ -2,6 +2,8 @@
 namespace App\Shop\Admin\Settings\Validators;
 
 use App\Shop\Admin\Settings\Validators\GeneralSettingsValidator;
+use App\Shop\Admin\Settings\Validators\AdminSettingsValidator;
+use Illuminate\Support\Facades\Validator;
 
 class ValidatorRouter
 {    
@@ -16,7 +18,30 @@ class ValidatorRouter
     {
         switch($data['target']) {
             case 'general_settings':
-                return (new GeneralSettingsValidator)->validate($data);
+                return (new GeneralSettingsValidator)->validateData($data);
+            case 'admin_settings':
+                return (new AdminSettingsValidator)->validateData($data);
+            case 'user_settings':
+                return (new UserSettingsValidator)->validateData($data);
         }
+    }
+
+    /**
+     * Data validation
+     *
+     * @param array $data
+     *
+     * @return array|bool
+     */
+    public function validateData(array $data): array|bool
+    {
+        $validator = Validator::make($data, $this->rules(), $this->messages());
+        if($validator->fails()) {
+            $errors = $validator->messages();
+            session()->flash('error_message', $errors->all());
+            return false;
+        }
+        unset($data['target']);
+        return $data;
     }
 }
