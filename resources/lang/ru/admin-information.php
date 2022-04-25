510ad74afd23334eb70bd78965596656fe8b9312
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-25 21:57:54 +0300

    Change lang lines for information module

--- resources/lang/ru/admin-information.php
+++ resources/lang/ru/admin-information.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'server-info' => '<strong>Информация о сервере:</strong>',
-    'php' => '<strong>Версия PHP:</strong>',
-    'memory' => '<strong>Доступный объем памяти:</strong>',
-    'execute-time' => '<strong>Макс. время выполнения:</strong>',
-    'max-upload-size' => '<strong>Макс. размер загрузки:</strong>',
-    'laravel-version' => '<strong>Версия laravel:</strong>',
-    'store-url' => '<strong>URL магазина:</strong>',
-    'current-template' => '<strong>Текущий шаблон:</strong> н/д',
-    'client' => '<strong>Ваш браузер:</strong>',
+    'information-server-info' => '<strong>Информация о сервере:</strong>',
+    'information-php' => '<strong>Версия PHP:</strong>',
+    'information-memory' => '<strong>Доступный объем памяти:</strong>',
+    'information-execute-time' => '<strong>Макс. время выполнения:</strong>',
+    'information-max-upload-size' => '<strong>Макс. размер загрузки:</strong>',
+    'information-laravel-version' => '<strong>Версия laravel:</strong>',
+    'information-store-url' => '<strong>URL магазина:</strong>',
+    'information-current-template' => '<strong>Текущий шаблон:</strong> н/д',
+    'information-client' => '<strong>Ваш браузер:</strong>',
 ];
