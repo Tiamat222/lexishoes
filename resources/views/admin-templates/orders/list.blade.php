d2447e65a685196d6b4d0009c352cb0f54015f09
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-28 23:16:51 +0300

    Orders displayed in the template

--- resources/views/admin-templates/orders/list.blade.php
+++ resources/views/admin-templates/orders/list.blade.php
@@ -24,14 +24,14 @@
             @include('admin-templates.info-messages')
             <div class="row">
               <div class="col-sm-12">
-                
+                @if(isset($orders) && count($orders) > 0)
                 <table id="example2" class="table table-bordered table-hover dataTable dtr-inline">
                   <thead>
                     <tr role="row">                  
                       <th class="th-middle">ID</th>
                       <th class="th-middle"># заказа</th>
                       <th class="th-middle">Заказчик</th>
-                      <th class="th-middle">Пункт доставки</th>
+                      <th class="th-middle">Адрес доставки</th>
                       <th class="th-middle">Итоговая цена</th>
                       <th class="th-middle">Статус заказа</th>
                       <th class="th-middle">Дата заказа</th>
@@ -39,20 +39,24 @@
                     </tr>
                   </thead>
                   <tbody>
-                    
+                    @foreach($orders as $order)
                     <tr role="row" class="odd">
+                      <td class="th-middle" style="width:40px;">{{ $order->id }}</td>
+                      <td class="th-middle" style="width:40px;">{{ $order->order_id }}</td>
                       <td class="th-middle"></td>
                       <td class="th-middle"></td>
-                      <td class="th-middle"></td>
-                      <td class="th-middle"></td>
-                      <td class="th-middle"></td>
-                      <td class="th-middle"></td>
-                      <td class="th-middle"></td>
-                      <td class="th-middle" style="width:130px;padding-left:16px;">
-                        <a class="btn btn-block btn-secondary btn-sm in-list-edit" href="" title="Детали заказа">
+                      <td class="th-middle" style="width:100px;">{{ $order->total_price }}</td>
+                      <td class="th-middle" style="width:250px;">
+                        @if($order->status === 0)
+                          <span class="new-order">Новый заказ</span>
+                        @endif
+                      </td>
+                      <td class="th-middle" style="width:200px;">{{ $order->created_at }}</td>
+                      <td class="th-middle" style="width:100px;padding-left:18px;">
+                        <a class="btn btn-block btn-secondary btn-sm in-list-edit" href="{{ route('admin.orders.edit', $order->id) }}" title="Детали заказа">
                           <i class="fas fa-eye"></i>
                         </a>
-                        <form action="" method="POST">
+                        <form action="{{ route('admin.orders.destroy', $order->id) }}" method="POST">
                           {{ csrf_field() }}
                           {{ method_field('DELETE') }}
                           <button type="submit" class="btn btn-block btn-secondary btn-sm in-list-del" title="Удалить заказ">
@@ -61,16 +65,21 @@
                         </form>
                       </td>
                     </tr>
-
+                    @endforeach
                   </tbody>
                 </table>
-
+                @else
+                <div class="callout callout-warning">
+                  <h5><i class="icon fas fa-info"></i> Внимание!</h5>
+                  Заказы отсутствуют в базе данных
+                </div>
+                @endif
               </div>
             </div>
             <div class="row">
               <div class="col-sm-12 col-md-7">
                 <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
-                  
+                  {{ $orders->onEachSide(1)->links('vendor.pagination.bootstrap-4') }}
                 </div>
               </div>
             </div>
