a68f56375ef13b42f533f0b9ffd46c9c51d316cb
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-28 22:14:47 +0300

    Added template for order list page

---
+++ resources/views/admin-templates/orders/list.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.admin-layouts.app')
+@section('title', 'Список заказов')
+@section('content')
+<div class="content-wrapper">
+@include('layouts.admin-layouts.content-header', ['h1' => 'Список заказов', 'breadcrumb' => 'orders'])
+  <section class="content">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title" style="margin-top:6px;">
+              <i class="fa fa-list-alt" aria-hidden="true"></i> Заказы
+            </h3>
+            <div style="float:right;">
+              <a href="" class="btn btn-block btn-default btn-sm a-trash" title="Корзина" style="width:130px;">
+                <i class="fa fa-trash"></i> Корзина ()
+              </a>
+              <a href="" class="btn btn-block btn-default btn-sm a-plus" title="Добавить заказ">
+                <i class="fas fa-plus"></i> Добавить заказ
+              </a>
+            </div>
+          </div>
+          <div class="card-body">
+            @include('admin-templates.info-messages')
+            <div class="row">
+              <div class="col-sm-12">
+                
+                <table id="example2" class="table table-bordered table-hover dataTable dtr-inline">
+                  <thead>
+                    <tr role="row">                  
+                      <th class="th-middle">ID</th>
+                      <th class="th-middle"># заказа</th>
+                      <th class="th-middle">Заказчик</th>
+                      <th class="th-middle">Пункт доставки</th>
+                      <th class="th-middle">Итоговая цена</th>
+                      <th class="th-middle">Статус заказа</th>
+                      <th class="th-middle">Дата заказа</th>
+                      <th class="th-middle" style="width:100px;">Действия</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    
+                    <tr role="row" class="odd">
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle"></td>
+                      <td class="th-middle" style="width:130px;padding-left:16px;">
+                        <a class="btn btn-block btn-secondary btn-sm in-list-edit" href="" title="Детали заказа">
+                          <i class="fas fa-eye"></i>
+                        </a>
+                        <form action="" method="POST">
+                          {{ csrf_field() }}
+                          {{ method_field('DELETE') }}
+                          <button type="submit" class="btn btn-block btn-secondary btn-sm in-list-del" title="Удалить заказ">
+                            <i class="fas fa-trash"></i>
+                          </button>
+                        </form>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12 col-md-7">
+                <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+@endsection
