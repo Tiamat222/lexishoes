f7c017c4c3711ea2388c29fc19474b2d0e42d3bf
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-29 23:35:52 +0300

    Add belongsToMany relationship to customers

--- app/Shop/Admin/Orders/Order.php
+++ app/Shop/Admin/Orders/Order.php
@@ -2,9 +2,11 @@
 
 namespace App\Shop\Admin\Orders;
 
+use App\Shop\Admin\Customers\Customer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Shop\Core\Admin\Traits\RecordFeed;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
@@ -34,9 +36,19 @@ class Order extends Model
         'total_price',
         'status',
     ];
-    
+
     public static function boot()
     {
         parent::boot();
+    }
+    
+    /**
+     * Order has customers
+     *
+     * @return BelongsToMany
+     */
+    public function customers(): BelongsToMany
+    {
+        return $this->belongsToMany(Customer::class);
     }
 }
