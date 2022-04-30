d052c42ee43cd531083da284d37b932571a60625
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-30 22:53:02 +0300

    Add new lang lines

--- resources/lang/ru/admin-orders.php
+++ resources/lang/ru/admin-orders.php
@@ -2,4 +2,6 @@
 
 return [
     'order-status-change-success' => 'Статус заказа был изменен',
+    'order-comment-required'      => 'Поле "Внутренний комментарий к заказу" обязательно к заполнению',
+    'order-save-comment-success'  => 'Новый комментарий к заказу был успешно добавлен',
 ];
