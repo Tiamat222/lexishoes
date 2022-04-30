51d745601cd25e14c4d4420b94ad4c9526f490e3
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-04-30 22:51:22 +0300

    Order has many comments

--- app/Shop/Admin/Orders/Order.php
+++ app/Shop/Admin/Orders/Order.php
@@ -3,10 +3,12 @@
 namespace App\Shop\Admin\Orders;
 
 use App\Shop\Admin\Customers\Customer;
+use App\Shop\Admin\OrderComments\OrderComment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Shop\Core\Admin\Traits\RecordFeed;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -41,7 +43,7 @@ class Order extends Model
     {
         parent::boot();
     }
-    
+
     /**
      * Order has customers
      *
@@ -50,5 +52,15 @@ class Order extends Model
     public function customers(): BelongsToMany
     {
         return $this->belongsToMany(Customer::class);
+    }
+
+    /**
+     * Order has comments
+     *
+     * @return HasMany
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(OrderComment::class);
     }
 }
