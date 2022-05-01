f43ddda78820a13e603f5e1fdcfa1ec27117ee03
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-01 21:18:06 +0300

    Created lang file for attributes values module

---
+++ resources/lang/ru/admin-attribute-values.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'attribute-value-name-required' => 'Поле "Значение" обязательно к заполнению',
+    'attribute-value-name-min' => 'Минимальная длина значения атрибута - 2 символа',
+    'attribute-value-attribute-required' => 'Атрибут не был выбран',
+    'attribute-value-create-success' => 'Атрибут был успешно создан',
+    'attribute-value-destroy-success' => 'Значение атрибута было успешно удалено',
+    'attribute-value-regex-hex' => 'Атрибуты "Цвет верха" и "Цвет подошвы" могут принимать значения только в HEX-формате (например, #ffffff)',
+    'attribute-value-update-success' => 'Значение атрибута было успешно обновлено'
+];
