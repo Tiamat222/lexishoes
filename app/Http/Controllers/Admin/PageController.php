192380aa5dd4bab4c639efdf2593b81ea1bc1091
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-02 11:09:05 +0300

    Added index method for outputting all pages

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\View\View;
+use App\Shop\Admin\Pages\Services\PageService;
+
 class PageController
-{
-    public function index()
+{    
+    /**
+     * PageService instance
+     *
+     * @var PageService
+     */
+    private $pageService;
+
+    /**
+     * PageController constructor
+     *
+     * @param  PageService $pageService
+     */
+    public function __construct(PageService $pageService)
     {
+        $this->pageService = $pageService;
+    }
+
+    /**
+     * Display a listing of the pages
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('admin-templates.catalog.pages.list', [
+            'pages' => $this->pageService->getAllRecordsPaginate(get_setting('items_per_page'))
+        ]);
     }
 }
