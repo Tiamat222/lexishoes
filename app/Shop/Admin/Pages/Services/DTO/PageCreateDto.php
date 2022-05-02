ce07037e58182d1fd5c4703a11dd9b7b7fd54da8
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-02 21:34:10 +0300

    Created DTO for creating new page

---
+++ app/Shop/Admin/Pages/Services/DTO/PageCreateDto.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Shop\Admin\Pages\Services\DTO;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+final class PageCreateDto extends DataTransferObject
+{    
+    /**
+     * Page title
+     *
+     * @var string
+     */
+    public string $title;
+
+    /**
+     * Page slug
+     *
+     * @var string
+     */
+    public string $slug;
+
+    /**
+     * Page text
+     *
+     * @var null|string
+     */
+    public ?string $text;
+
+    /**
+     * Page meta title
+     *
+     * @var null|string
+     */
+    public ?string $metaTitle;
+
+    /**
+     * Page meta keywords
+     *
+     * @var null|string
+     */
+    public ?string $metaKeywords;
+
+    /**
+     * Page meta description
+     *
+     * @var null|string
+     */
+    public ?string $metaDescription;
+}
