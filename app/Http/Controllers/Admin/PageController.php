adb7d7fa6d5d2a73cd08d30e699ff7a2bfc5d0b7
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-02 21:23:43 +0300

    New page can be stored

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Shop\Admin\Pages\Requests\CreatePageRequest;
 use Illuminate\View\View;
 use App\Shop\Admin\Pages\Services\PageService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PageController
 {    
@@ -34,5 +39,40 @@ class PageController
         return view('admin-templates.catalog.pages.list', [
             'pages' => $this->pageService->getAllRecordsPaginate(get_setting('items_per_page'))
         ]);
+    }
+
+    /**
+     * Showing the form for adding a new category
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('admin-templates.catalog.pages.create');
+    }
+    
+    /**
+     * Store new page
+     *
+     * @param  CreatePageRequest $request
+     * 
+     * @return RedirectResponse
+     */
+    public function store(CreatePageRequest $request): RedirectResponse
+    {
+        $this->pageService->store($request->data());
+        return redirect()
+                ->route('admin.catalog.pages.index')
+                ->with('success_message', __('admin-pages.pages-store-success'));
+    }
+    
+    /**
+     * Create page slug (AJAX)
+     *
+     * @return JsonResponse
+     */
+    public function createSlug(Request $request): JsonResponse
+    {
+        return response()->json(['slug' => Str::slug($request['pageTitle'], '-')]);
     }
 }
