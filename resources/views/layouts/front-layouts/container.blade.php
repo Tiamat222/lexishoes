c7fc9cb2172e760e4ab533f2ec444bf5e03c0642
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-03 14:42:05 +0300

    Removing old files (template replacement)

--- resources/views/layouts/front-layouts/container.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-  <!-- Content Wrapper. Contains page content -->
-  @yield('content')
-  <!-- Sidebar -->
-  @include('layouts.front-layouts.sidebar')
-	<div class="clearfix"></div>        	         
-</div>
