3ed0a5f64b6837bebc946ff5c443dcf061e4e1d0
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-11 10:43:00 +0300

    Created lang file fore customer login module

---
+++ resources/lang/ru/front-login-customer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'customer-email-required' => 'Вы не ввели email.',
+    'customer-email-email' => 'Вы ввели некорректный email.',
+    'customer-email-exists' => 'Вы ввели неправильный email/пароль.',
+    'customer-password-required' => 'Вы не ввели пароль.',
+    'customer-logout' => 'Выполнен выход из аккаунта.',
+    'customer-login-warning' => 'Доступ к аккаунту ограничен. Вы не подтвердили email/доступ ограничен администратором.'
+];
