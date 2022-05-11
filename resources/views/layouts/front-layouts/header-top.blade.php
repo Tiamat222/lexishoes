ee85de839567690965cb163f977c1c4c6b6ed888
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-11 10:30:58 +0300

    Changed header-top template

--- resources/views/layouts/front-layouts/header-top.blade.php
+++ resources/views/layouts/front-layouts/header-top.blade.php
@@ -1,25 +1,35 @@
-<div class="header_top"><!--header_top-->
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="contactinfo">
-							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
-							</ul>
-						</div>
+<div class="header_top">
+  <div class="container">
+	<div class="row">
+	  <div class="col-sm-12">
+	    <div class="col-xs-6 col-md-8">
+		  <div class="search_box">
+		    <form action="" method="GET">
+			  <div class="row">
+				<div class="col-md-10">
+			      <div class="input-group">
+					<input type="text" class="form-control" placeholder="Поиск..." id="txtSearch"/>
+					<div class="input-group-btn">
+					  <button class="btn btn-search" type="submit">
+					    <span class="glyphicon glyphicon-search"></span>
+					  </button>
 					</div>
-					<div class="col-sm-6">
-						<div class="social-icons pull-right">
-							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							</ul>
-						</div>
-					</div>
+				  </div>
 				</div>
-			</div>
-		</div><!--/header_top-->
+			  </div>
+			</form>
+		  </div>
+		</div>
+		<div class="col-sm-4 links-right">
+		  <div class="links">
+			@if(check_auth_user('customers'))
+			<a href="{{ route('front.login.logout_customer') }}"><i class="fa fa-sign-out" aria-hidden="true" title="Выйти"></i></a>
+			@endif
+			<a href="{{ (check_auth_user('customers')) ? route('front.customer.profile') : route('front.login.show_form') }}" title="Войти"><i class="fa fa-user"></i></a>
+			<a href="cart.html"><i class="fa fa-shopping-cart"></i></a>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </div>
+</div>
