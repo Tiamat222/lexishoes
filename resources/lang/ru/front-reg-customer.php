237d942f7ea02caf8a1378b68ff7a2cb696ab52c
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-05-14 08:44:02 +0300

    Created new lang file for the new customer registration module

---
+++ resources/lang/ru/front-reg-customer.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'customer-reg-subject'        => 'Регистрация на lexishoes.com.ua',
+    'customer-email-required'     => 'Вы не указали email.',
+    'customer-email-email'        => 'Вы ввели некорректный email.',
+    'customer-email-exists'       => 'Введенный Вами email отличается от того, который вы указывали при регистрации.',
+    'customer-reg-success'        => 'Поздравляем с успешной регистрацией! Теперь Вы может зайти в свой аккаунт.',
+    'customer-firstName-required' => 'Вы не указали свое имя.',
+    'customer-lastName-required'  => 'Вы не указали свою фамилию.',
+    'customer-email-unique'       => 'Пользователь с таким email уже зарегистрирован.',
+    'customer-phone-required'     => 'Вы не указали свой номер телефона.',
+    'customer-phone-regex'        => 'Номер телефона должен соответствовать формату +38(ХХХ) ХХХ-ХХ-ХХ.',
+    'customer-phone-unique'       => 'Пользователь с таким номером телефона уже зарегистрирован.',
+    'customer-password-required'  => 'Вы не указали пароль.',
+    'customer-password-min'       => 'Минимальная длина пароля - ' . get_setting('pwd_length') . ' символов(а)',
+
+];
