a07fe7fc9816a63b9dc973d42ef445abfe8c77ff
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-06-27 20:29:04 +0300

    Created new controller for store pages module

---
+++ app/Http/Controllers/Front/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Shop\Admin\Pages\Services\PageService;
+use Illuminate\View\View;
+
+class PageController extends Controller
+{    
+    /**
+     * Show page content
+     *
+     * @param  string $slug
+     * 
+     * @return View
+     */
+    public function showPage(string $slug, PageService $pageService): View
+    {
+        return view('front-templates.page', [
+            'page' => $pageService->getRecordByField('slug', $slug)
+        ]);
+    }
+}
+
