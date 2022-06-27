38ed8a558e778faf73bdde0a557a2c5ca24c232e
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-06-27 20:31:05 +0300

    Created new service provider for preload front template data

---
+++ app/Providers/FrontTemplateServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use App\Shop\Admin\Pages\Services\PageService;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Collection;
+
+class FrontTemplateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(PageService $pageService)
+    {
+        // Outputting store pages in header
+        if(Schema::hasTable('pages')) {
+            $pagesList = new Collection();
+            foreach($pageService->getAllRecords() as $page) {
+                if($page->status !== 0) {
+                    $pagesList->push($page);
+                }
+            }
+            view()->share('pages', pluck_collection_to_array($pagesList, 'title', 'slug'));
+        }
+    }
+}
