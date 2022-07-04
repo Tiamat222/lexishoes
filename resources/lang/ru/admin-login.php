249211c93bf18a0ad30f422af1e6c0c0a294f433
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-07-04 08:42:43 +0300

    Created lang file for admin auth module

---
+++ resources/lang/ru/admin-login.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'admin-auth-warning' => 'Доступ к админ-панели ограничен администратором.',
+    'admin-login-required' => 'Вы не ввели логин.',
+    'admin-login-exists' => 'Вы ввели неверный логин/пароль.',
+    'admin-password-required' => 'Вы не ввели пароль.',
+];
