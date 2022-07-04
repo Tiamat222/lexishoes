20e594cf917ce8588cd1ea789d82cb954974efe7
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-07-04 08:36:52 +0300

    Admin login template was changed

--- resources/views/admin-templates/login.blade.php
+++ resources/views/admin-templates/login.blade.php
@@ -1,52 +1,52 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | Log in</title>
+    <title>Вход в админ-панель</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ url('admin-template/plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ url('admin-template/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('admin-template/dist/css/adminlte.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   </head>
-<body class="login-page" style="min-height: 512.781px;">
-<div class="login-box">
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Вход в админ панель</p>
-      @include('admin-templates.info-messages')
-      <form action="{{ route('admin.check') }}" method="post">
-        {{ csrf_field() }}
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Ваш логин" name="login">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+  <body class="login-page" style="min-height: 512.781px;">
+    <div class="login-box">
+      <div class="card">
+        <div class="card-body login-card-body" style="border-radius: 15px;">
+          <p class="login-box-msg">Вход в админ панель</p>
+          @include('admin-templates.info-messages')
+          <form action="{{ route('admin.check') }}" method="post">
+            {{ csrf_field() }}
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Логин" name="login" required>
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Ваш пароль" name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" placeholder="Пароль" name="password" required>
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <a href="{{ route('admin.forget.get') }}">Забыл(а) пароль</a>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <a href="{{ route('admin.forgot.get') }}">Забыл(а) пароль</a>
+                </div>
+              </div>
+              <div class="col-4">
+                <button type="submit" class="btn btn-block btn-secondary">Войти</button>
+              </div>
             </div>
-          </div>
-          <div class="col-4">
-            <button type="submit" class="btn btn-block btn-secondary">Войти</button>
-          </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
-  </div>
-</div>
-</body>
+  <script src="{{ url('admin-template/plugins/jquery/jquery.min.js') }}"></script>
+  <script src="{{ url('admin-template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ url('admin-template/dist/js/adminlte.js') }}"></script>
+  </body>
 </html>
