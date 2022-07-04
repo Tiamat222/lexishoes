099865718df4042f4c7d5894ff8b3fe2db8969fe
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-07-04 08:33:44 +0300

    Administrator can authenticated

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -3,12 +3,64 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Shop\Admin\Login\Requests\LoginRequest;
+use App\Shop\Admin\Login\Services\LoginService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class LoginController extends Controller
 {
-    public function showLoginForm()
+    /**
+     * LoginService instance
+     *
+     * @var LoginService
+     */
+    protected $loginService;
+
+    /**
+     * LoginController constructor
+     *
+     * @param  LoginService $loginService
+     */
+    public function __construct(LoginService $loginService)
+    {
+        $this->loginService = $loginService;
+    }
+
+    /**
+     * Show page with login form
+     *
+     * @return View
+     */
+    public function showLoginForm(): View
     {
         return view('admin-templates.login');
+    }
+
+    /**
+     * Admin auth
+     *
+     * @param  Request $request
+     * 
+     * @return RedirectResponse
+     */
+    public function adminCheck(LoginRequest $request): RedirectResponse
+    {
+        if($this->loginService->userLoginAttempt(['login' => $request->login, 'password' => $request->password], 'admins')) {
+            return redirect()->route('admin.dashboard');
+        }
+        return redirect()->route('admin.login');
+    }
+
+    /**
+     * Admin logout
+     *
+     * @return RedirectResponse
+     */
+    public function adminLogout(): RedirectResponse
+    {
+        $this->loginService->logout('admins');
+        return redirect()->route('admin.login');
     }
 }
