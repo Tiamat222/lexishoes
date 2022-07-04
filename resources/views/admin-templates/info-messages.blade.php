64494dbc6354ac125d5266c4620a68dd6ec7901b
Evgeny Shekhurin <shehurinevgeny@gmail.com>
2022-07-04 08:38:26 +0300

    Minor changes

--- resources/views/admin-templates/info-messages.blade.php
+++ resources/views/admin-templates/info-messages.blade.php
@@ -1,36 +1,36 @@
 @if($message = Session::get('warning_message'))
-<div class="alert alert-warning alert-dismissible">
+<div class="alert callout callout-warning alert-dismissible" style="background: #EFDE92;">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
   <h5><i class="icon fas fa-exclamation-triangle"></i> Внимание!</h5>
   {{ $message }}
 </div>
 @endif
 @if($message = Session::get('error_message'))
-<div class="alert callout callout-danger" style="background:#FF8080;">
+<div class="alert callout callout-danger" style="background:#F5CDCD;">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
   <h5><i class="icon fas fa-ban"></i>Ошибка!</h5>
-    @if(is_array($message) && count($message) >= 1)
-      @foreach($message as $errorMessage)
-        - {{ $errorMessage }}<br>
-      @endforeach
-    @else 
-      {{ $message }}
-    @endif
+  @if(is_array($message) && count($message) >= 1)
+    @foreach($message as $errorMessage)
+      - {{ $errorMessage }}<br>
+    @endforeach
+  @else
+    {{ $message }}
+  @endif
 </div>
 @endif
 @if($message = Session::get('success_message'))
-<div class="alert callout callout-success" style="background:#85BF73;">
+<div class="alert callout callout-success" style="background:#B7F5B1;">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
   <h5><i class="icon fas fa-check"></i> Успех!</h5>
-  {{ $message }}         
+  {{ $message }}
 </div>
 @endif
 @if($errors->any())
-<div class="alert callout callout-danger" style="background:#FF8080;">
+<div class="alert callout callout-danger" style="background:#F5CDCD;">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
   <h5><i class="icon fas fa-ban"></i>Ошибка!</h5>
-    @foreach($errors->all() as $error)
-        - {{ $error }}</br>
-    @endforeach
+  @foreach($errors->all() as $error)
+    - {{ $error }}</br>
+  @endforeach
 </div>
 @endif
